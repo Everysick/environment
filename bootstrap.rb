b9f6fd3ba83b4ae6fe8d950f7e8297cf986b23b7
everysick <s.wakeup31@gmail.com>
2016-11-25 06:44:56 -0800

    Update bootsrap.rb

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,5 +1,17 @@
+MItamae::RecipeContext.class_eval do
+  def include_cookbook(name)
+    root_dir = File.expand_path('..', __FILE__)
+    include_recipe File.join(root_dir, 'cookbooks', name, 'default')
+  end
+
+  def include_role(name)
+    root_dir = File.expand_path('..', __FILE__)
+    include_recipe File.join(root_dir, 'roles', name, 'default')
+  end
+end
+
 node['roles'] = node['roles'] || []
 
 node['roles'].each do |role|
-  include_recipe "roles/#{role}/default.rb"
+  include_role role
 end
